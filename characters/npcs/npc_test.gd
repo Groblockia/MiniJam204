2e38fca4fc1639e73460dff209aa199b3a64564a
Groblockia <groblovroum@gmail.com>
2026-02-13 09:40:33 +0100

    added anim/model

--- characters/npcs/npc_test.gd
+++ characters/npcs/npc_test.gd
@@ -7,11 +7,6 @@ const SPEED := 5.0
 func _physics_process(delta: float) -> void:
 	velocity.y -= 9.8 * delta
 	move_and_slide()
-	
-	if Input.is_action_just_pressed("interact"):
-		navAgent.target_position = get_target_location()
-		print(navAgent.target_position)
-	
 	move_to_target()
 
 func move_to_target():
@@ -20,7 +15,7 @@ func move_to_target():
 	var direction = (next_pos - current_pos).normalized()
 	velocity = direction * SPEED
 
-func get_target_location() -> Vector3:
+func set_target_location() -> Vector3:
 	var offset_x = randf_range(-10, 10) 
 	var offset_z = randf_range(-10, 10)
 	
